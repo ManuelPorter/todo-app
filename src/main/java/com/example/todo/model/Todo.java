48814c564dedc7ea375d7a150c2512f811d8e79c
Manuel Rivera <manuelrivera@kms-technology.com>
2026-04-13 19:27:20 -0600

    Security: Add JsonIgnore to UserId exposure 1

--- src/main/java/com/example/todo/model/Todo.java
+++ src/main/java/com/example/todo/model/Todo.java
@@ -1,5 +1,6 @@
 package com.example.todo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
@@ -20,6 +21,7 @@ public class Todo {
 
     private LocalDateTime createdAt;
     private LocalDateTime dueAt;
+    @JsonIgnore
     private Long userId;
 
     @PrePersist
